38c0253ac53c3d5ccb77d4fb1b033e61941810db
sacrwst <116656490+sacrwst@users.noreply.github.com>
2023-08-20 22:20:29 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import '@/styles/globals.css'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-7xl">Vite + React + TypeScript + Tailwind CSS</h1>
+    </>
+  )
+}
+
+export default App
